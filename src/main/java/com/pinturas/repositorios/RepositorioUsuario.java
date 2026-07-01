192d20c3e33cda976331a529cce22542950c6e14
Evelyn Alvarez Vasquez <EVELYNTEC@gmail.com>
2026-07-01 14:33:35 -0400

    Delete src directory

--- src/main/java/com/pinturas/repositorios/RepositorioUsuario.java
+++
@@ -1,9 +0,0 @@
-package com.pinturas.repositorios;
-
-import com.pinturas.modelos.Usuario;
-import org.springframework.data.repository.CrudRepository;
-import java.util.Optional;
-
-public interface RepositorioUsuario extends CrudRepository<Usuario, Long> {
-    Optional<Usuario> findByCorreo(String correo);
-}
